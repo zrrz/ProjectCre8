fe2e5a3a87dbbe2aaf19769c1d6a1ff08d4c5401
Zack Rock <zacharyrock@hotmail.com>
2017-02-19 03:32:16 -0800

    first commit

---
+++ Assets/Scripts/Designer/Node.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.UI.Extensions;
+
+namespace Designer {
+	public class Node : MonoBehaviour, IDragHandler {
+
+		public Node next;
+		public Node prev;
+
+		Connectable anchorPrev;
+		Connectable AnchorPrev { get { return anchorPrev;} }
+		Connectable anchorNext;
+		Connectable AnchorNext { get { return anchorNext;} }
+
+		UILineRenderer line;
+
+		void Start () {
+			line = gameObject.GetComponentInChildren<UILineRenderer>();
+
+			anchorPrev = transform.FindChild("Body").FindChild("FlowAnchorPrev").GetComponent<Connectable>();
+			anchorNext = transform.FindChild("Body").FindChild("FlowAnchorNext").GetComponent<Connectable>();
+		}
+
+		void Update () {
+			if(next) {
+				line.enabled = true;
+				line.Points = new Vector2[] {
+					new Vector2(0f, 0f), 
+					new Vector2(30f, 0), 
+					RectTransformExtension.switchToRectTransform(next.AnchorPrev.GetComponent<RectTransform>(), line.GetComponent<RectTransform>()) - new Vector2(30f, 0),
+					RectTransformExtension.switchToRectTransform(next.AnchorPrev.GetComponent<RectTransform>(), line.GetComponent<RectTransform>())
+				};
+			} else {
+				line.enabled = false;
+			}
+		}
+
+		public void OnDrag(PointerEventData data) {
+			GetComponent<RectTransform>().anchoredPosition += data.delta;
+		}
+	}
+}
